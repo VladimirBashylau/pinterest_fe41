bab7e4988406733a21a188ec00725122bf0b2f48
VladimirBashylau <vladimirbvv01@yandex.ru>
2023-07-25 20:00:08 +0300

    styled PostItem

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import "./App.css";
 import PostItem from "./components/PostItem/PostItem";
 
 function App() {
-  return (
-    <>
-      <PostItem />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
